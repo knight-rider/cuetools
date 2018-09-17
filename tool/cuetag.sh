af6a09e05101d0bcfd94e2cfad13fea032508e57
Буди Романто, AreMa Inc <knightrider@are.ma>
2018-09-18 08:20:20 +0900

    unified output

--- tool/cuetag.sh
+++ tool/cuetag.sh
@@ -4,12 +4,13 @@
 # uses cueprint output
 # usage: cuetag.sh <cuefile|tocfile> [file]...
 
-CUEPRINT=cueprint
+CUEPRINT=`which cueprint`
 cue_file=""
 
 # print usage instructions
 usage()
 {
+	echo
 	echo "usage: cuetag.sh <cuefile|tocfile> [file]..."
 	echo
 	echo "cuetag.sh adds tags to files based on cue or toc information"
@@ -173,7 +174,7 @@ id3()
 
 main()
 {
-	if [ $# -lt 1 ]; then
+	if [[ $# -lt 1 || -z $CUEPRINT ]]; then
 		usage
 		exit
 	fi
@@ -181,7 +182,7 @@ main()
 	cue_file=$1
 	shift
 
-	ntrack=$(cueprint -d '%N' "$cue_file")
+	ntrack=$($CUEPRINT -d '%N' "$cue_file")
 	trackno=1
 
 	NUM_FILES=0 FIELDS=
@@ -197,18 +198,24 @@ main()
 	fi
 
 	for file in "$@"; do
+		LBL=`$CUEPRINT -n $trackno -t '%02n %p - %t' "$cue_file"`
+
 		case $file in
 		*.[Ff][Ll][Aa][Cc])
 			vorbis $trackno "$file" $FIELDS
+			[ "$file" != "$LBL.flac" ] && `mv "$file" "$LBL.flac"`
 			;;
 		*.[Oo][Gg][Gg])
 			vorbis $trackno "$file" $FIELDS
+			[ "$file" != "$LBL.ogg" ] && `mv "$file" "$LBL.ogg"`
 			;;
 		*.[Mm][Pp]3)
 			id3 $trackno "$file" $FIELDS
+			[ "$file" != "$LBL.mp3" ] && `mv "$file" "$LBL.mp3"`
 			;;
 		*.[Tt][Xx][Tt])
 			vorbis $trackno "$file"
+			[ "$file" != "$LBL.txt" ] && `mv "$file" "$LBL.txt"`
 			;;
 		*.*)
 			echo "$file: uknown file type"
